297338d533df71eafb3cfc8d15bfb68b43d9d720
Flor <florxgomez@gmail.com>
2019-06-05 20:15:37 -0300

    changing /

--- server.js
+++ server.js
@@ -24,7 +24,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send(db.select('*').from('users')) })
+app.get('/', (req, res) => res.send('it is working!'))
+//app.get('/', (req, res) => { res.send(db.select('*').from('users')) })
 app.post('/signIn', handleSignIn( db, bcrypt))
 app.post('/register', (req, res) => { handleRegister(req, res, db, bcrypt) });
 app.get('/profile/:id', (req, res) => { handleProfile(req, res, db) });
